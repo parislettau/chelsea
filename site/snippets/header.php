5c1675196b37d2178902cb065a62378e41e2f809
parislettau <paris.lettau@gmail.com>
2021-12-06 09:54:11 +1100

    background

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,7 +8,7 @@
 <body 
    data-display="<?= $page->template() ?>" 
    class="<?php if($page->template() == 'exhibition' || $page->template() == 'page'): ?>content-pane-open<?php endif ?>"
-    style="background-image: url('<?= $site->background()->toFile()->url() ?>')"
+    style="background-size: cover;background-repeat: no-repeat;background-position: center;background-image: url('<?= $site->background()->toFile()->url() ?>')"
     >
       <?php snippet('nav') ?>
       <?php snippet('home') ?>
