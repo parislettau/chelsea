2a0acb67e0854ff6cd10cac483c97a54da82a9f5
parislettau <paris.lettau@gmail.com>
2021-12-09 10:06:46 +1100

    Paris's MacBook M1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+cd ~/sites/ninetyninepercent/
+
 /bin/bash <<EOF
   git add .
   git commit -m "Paris's MacBook M1"
@@ -17,3 +19,8 @@ ssh -A $USER@$HOST /bin/bash <<EOF
   sudo chown -R www-data:www-data .
 
 EOF
+
+HOST=root@45.63.28.222
+WEBROOT=/var/www/html/ninetyninepercent
+
+rsync -r -p -t -u -z --checksum --exclude=".*" -P -h -i --delete $HOST:$WEBROOT/content ./
