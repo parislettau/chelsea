99af02be7f3cbc0d4b74ccf8e5d0e1f04d4d7f3a
Paris Lettau <55466163+parislettau@users.noreply.github.com>
2022-12-18 10:37:42 +1100

    redirects

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,7 +1,17 @@
-<?php snippet('header') ?>
-<main data-template="<?= $page->template() ?>">
-<?php snippet('exhibitions') ?>
+<meta name=”robots” content=”noindex”>
 
+<?php if ($kirby->user()) : ?>
+    <?php if ($page->status() == 'draft') : ?>
+        <p>Please change page status from <strong style="color: red;">“Draft”</strong> to <strong style="color: green">“Public”</strong> before previewing.</p>
 
-</main>
-<?php snippet('footer') ?>
+    <?php else : ?>
+
+        <head>
+            <meta http-equiv="refresh" content="0; url='https://99percent.gallery/<?= $page->id() ?>'" />
+        </head>
+
+        <body>
+            <p>Please follow <a href="https://99percent.gallery/<?= $page->id() ?>">this link</a> to preview the page.</p>
+        </body>
+    <?php endif ?>
+<?php endif ?>
