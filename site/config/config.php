c87ee1305af9e50ea2f19ab6aa575225821178eb
parislettau <55466163+parislettau@users.noreply.github.com>
2022-12-19 10:05:53 +1100

    webp

--- site/config/config.php
+++ site/config/config.php
@@ -21,11 +21,17 @@ return [
   //   'install' => true
   // ],
 
+  'markdown' => [
+    'extra' => true
+  ],
+  'smartypants' => true,
+
   // Srcsets
   'thumbs' => [
     'srcsets' => [
       'driver' => 'im',                                       // https://getkirby.com/docs/guide/troubleshooting/thumbnails
       // 'bin' => '/usr/local/bin/convert',                   // https://getkirby.com/docs/guide/troubleshooting/thumbnails
+      'format' => 'webp',
       'srcsets' => [                                          // https://getkirby.com/docs/cookbook/performance/responsive-images
         'default' => [
           '300w'  => ['width' => 300],
@@ -34,20 +40,20 @@ return [
           '1200w' => ['width' => 1200],
           '1800w' => ['width' => 1800]
         ],
-        'avif' => [
-          '300w'  => ['width' => 300, 'format' => 'avif'],
-          '600w'  => ['width' => 600, 'format' => 'avif'],
-          '900w'  => ['width' => 900, 'format' => 'avif'],
-          '1200w' => ['width' => 1200, 'format' => 'avif'],
-          '1800w' => ['width' => 1800, 'format' => 'avif']
-        ],
-        'webp' => [
-          '300w'  => ['width' => 300, 'format' => 'webp'],
-          '600w'  => ['width' => 600, 'format' => 'webp'],
-          '900w'  => ['width' => 900, 'format' => 'webp'],
-          '1200w' => ['width' => 1200, 'format' => 'webp'],
-          '1800w' => ['width' => 1800, 'format' => 'webp'],
-        ],
+        // 'avif' => [
+        //   '300w'  => ['width' => 300, 'format' => 'avif'],
+        //   '600w'  => ['width' => 600, 'format' => 'avif'],
+        //   '900w'  => ['width' => 900, 'format' => 'avif'],
+        //   '1200w' => ['width' => 1200, 'format' => 'avif'],
+        //   '1800w' => ['width' => 1800, 'format' => 'avif']
+        // ],
+        // 'webp' => [
+        //   '300w'  => ['width' => 300, 'format' => 'webp'],
+        //   '600w'  => ['width' => 600, 'format' => 'webp'],
+        //   '900w'  => ['width' => 900, 'format' => 'webp'],
+        //   '1200w' => ['width' => 1200, 'format' => 'webp'],
+        //   '1800w' => ['width' => 1800, 'format' => 'webp'],
+        // ],
       ]
     ]
   ]
