2ffc3759d20a3e44bceb6f67f9b196cffdd3da0b
parislettau <55466163+parislettau@users.noreply.github.com>
2022-12-19 10:19:05 +1100

    fix

--- site/config/config.php
+++ site/config/config.php
@@ -27,45 +27,32 @@ return [
   'smartypants' => true,
 
   // Srcsets
-  'thumbs' => [
-    'driver' => 'im',
+  'thumbs' => [                                             // https://getkirby.com/docs/cookbook/performance/responsive-images
+    'driver' => 'im',                                       // https://getkirby.com/docs/guide/troubleshooting/thumbnails
+    // 'bin' => '/usr/local/bin/convert',                   // https://getkirby.com/docs/guide/troubleshooting/thumbnails
     'srcsets' => [
-      'quality' => 85,
       'default' => [
-        '800w' => ['width' => 800],
-        '1440w' => ['width' => 1440],
-        '2048w' => ['width' => 2048]
-      ]
+        '300w'  => ['width' => 300],
+        '600w'  => ['width' => 600],
+        '900w'  => ['width' => 900],
+        '1200w' => ['width' => 1200],
+        '1800w' => ['width' => 1800]
+      ],
+      'avif' => [
+        '300w'  => ['width' => 300, 'format' => 'avif'],
+        '600w'  => ['width' => 600, 'format' => 'avif'],
+        '900w'  => ['width' => 900, 'format' => 'avif'],
+        '1200w' => ['width' => 1200, 'format' => 'avif'],
+        '1800w' => ['width' => 1800, 'format' => 'avif']
+      ],
+      'webp' => [
+        '300w'  => ['width' => 300, 'format' => 'webp'],
+        '600w'  => ['width' => 600, 'format' => 'webp'],
+        '900w'  => ['width' => 900, 'format' => 'webp'],
+        '1200w' => ['width' => 1200, 'format' => 'webp'],
+        '1800w' => ['width' => 1800, 'format' => 'webp'],
+      ],
     ]
-  ],
-  // 'thumbs' => [
-  //   'srcsets' => [
-  //     // 'driver' => 'im',                                       // https://getkirby.com/docs/guide/troubleshooting/thumbnails
-  //     // 'bin' => '/usr/local/bin/convert',                   // https://getkirby.com/docs/guide/troubleshooting/thumbnails
-  //     // 'format' => 'webp',
-  //     'srcsets' => [                                          // https://getkirby.com/docs/cookbook/performance/responsive-images
-  //       'default' => [
-  //         '300w'  => ['width' => 300],
-  //         '600w'  => ['width' => 600],
-  //         '900w'  => ['width' => 900],
-  //         '1200w' => ['width' => 1200],
-  //         '1800w' => ['width' => 1800]
-  //       ],
-  //       // 'avif' => [
-  //       //   '300w'  => ['width' => 300, 'format' => 'avif'],
-  //       //   '600w'  => ['width' => 600, 'format' => 'avif'],
-  //       //   '900w'  => ['width' => 900, 'format' => 'avif'],
-  //       //   '1200w' => ['width' => 1200, 'format' => 'avif'],
-  //       //   '1800w' => ['width' => 1800, 'format' => 'avif']
-  //       // ],
-  //       // 'webp' => [
-  //       //   '300w'  => ['width' => 300, 'format' => 'webp'],
-  //       //   '600w'  => ['width' => 600, 'format' => 'webp'],
-  //       //   '900w'  => ['width' => 900, 'format' => 'webp'],
-  //       //   '1200w' => ['width' => 1200, 'format' => 'webp'],
-  //       //   '1800w' => ['width' => 1800, 'format' => 'webp'],
-  //       // ],
-  //     ]
-  //   ]
-  // ]
+  ]
+
 ];
