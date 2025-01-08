58e196306c8ab672fa0f1ec0637171fef2c737f7
Paris Lettau <55466163+parislettau@users.noreply.github.com>
2025-01-08 11:28:38 +1100

    Update

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-PROJECT=${PWD##*/}  # get project directory name
-HOST=ubuntu@67.219.98.22
-WEBROOT=/var/www/$PROJECT
+HOST=ubuntu@45.32.247.37
+WEBROOT=/etc/easypanel/projects/chelsea-hopper/99-percent/volumes/
 
-rsync -r -p -t -u -z --checksum --exclude=".*" -P -h -i --delete $HOST:$WEBROOT/content ./
+# Run rsync with refined options
+rsync -r -p -t -z --checksum -P -h -i --delete $HOST:$WEBROOT/content ./
